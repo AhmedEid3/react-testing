f65020e246017f7fe926774f16efd490d107abc2
Ahmed Eid <ahmed.eid3@outlook.com>
2025-09-25 07:16:28 +0300

    feat: initialize project structure with essential components and pages
    - Added entity types for User, Category, and Product.
    - Created environment type definitions for Auth0 configuration.
    - Implemented custom hooks for cart management, category fetching, language context, and product details.
    - Developed main application layout and routing structure.
    - Created pages for browsing products, product details, and admin functionalities.
    - Integrated Redux for state management and added category slice.
    - Set up Tailwind CSS for styling and configured global styles.
    - Established language provider with English and Spanish translations.
    - Added validation schema for product forms using Zod.
    - Configured Vite for development and testing environments.

---
+++ src/components/ErrorMessage.tsx
@@ -0,0 +1,14 @@
+import { FieldError } from 'react-hook-form'
+import { Text } from '@radix-ui/themes'
+
+const ErrorMessage = ({ error }: { error: FieldError | undefined }) => {
+  if (!error) return null
+
+  return (
+    <Text color='red' as='div' role='alert' data-for={error.ref!.name}>
+      {error.message}
+    </Text>
+  )
+}
+
+export default ErrorMessage
